f72abe6e04fefa1fe31b32f9b88a4f724629d5cc
hasibthedream <zarekhasib@gmail.com>
2026-02-24 13:51:10 -0600

    Add new routes for OilGas, Insurance, and more

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/Home';
 import SolarPage from './pages/Solar';
 import HealthcarePage from './pages/Healthcare';
+import OilGasPage from './pages/OilGas';
+import InsurancePage from './pages/Insurance';
+import CommercialRealEstatePage from './pages/CommercialRealEstate';
+import IndustrialPage from './pages/Industrial';
 
 export default function App() {
   return (
@@ -10,6 +14,10 @@ export default function App() {
       <Route path="/" element={<HomePage />} />
       <Route path="/solar" element={<SolarPage />} />
       <Route path="/healthcare" element={<HealthcarePage />} />
+      <Route path="/oil-gas" element={<OilGasPage />} />
+      <Route path="/insurance" element={<InsurancePage />} />
+      <Route path="/commercial-real-estate" element={<CommercialRealEstatePage />} />
+      <Route path="/industrial" element={<IndustrialPage />} />
     </Routes>
   );
 }
